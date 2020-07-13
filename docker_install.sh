2e4f20cc872232e973356098fb4aae563951888e
phenixzr <phoenixzr@hotmail.fr>
2020-07-13 21:16:16 +0100

    docker_install for RPI4

---
+++ docker_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt install -y --no-install-recommends docker-ce cgroupfs-mount
+sudo systemctl enable docker
+sudo systemctl start docker
